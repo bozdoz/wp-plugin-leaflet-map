bdd64e0b648388f4bb7ec6417683698439636673
bozdoz <howaboutben@gmail.com>
2020-04-04 22:50:41 -0300

    adds prettier and lazy initialization of ajax and svg functions

--- scripts/leaflet-ajax-geojson.js
+++ scripts/leaflet-ajax-geojson.js
@@ -1,70 +1,75 @@
-L.AjaxGeoJSON = L.GeoJSON.extend({
-    options : {
-        type: 'json' // 'json|kml|gpx'
-    },
+;(function () {
+  window.WPLeafletMapPlugin = window.WPLeafletMapPlugin || []
+  window.WPLeafletMapPlugin.unshift(initAjaxGeoJSON)
 
-    initialize : function (url, options) {
-        L.setOptions(this, options);
-        this._url = url;
-        this.layer = L.geoJson(null, this.options);
-    },
+  function initAjaxGeoJSON() {
+    L.AjaxGeoJSON = L.GeoJSON.extend({
+      options: {
+        type: 'json', // 'json|kml|gpx'
+      },
 
-    onAdd : function (map) {
-        var _this = this,
-            type = this.options.type,
-            xhr;
+      initialize: function (url, options) {
+        L.setOptions(this, options)
+        this._url = url
+        this.layer = L.geoJson(null, this.options)
+      },
 
-        this.map = map;
+      onAdd: function (map) {
+        var _this = this
+        var type = this.options.type
+        var xhr
 
-        map.addLayer( this.layer );
+        this.map = map
+
+        map.addLayer(this.layer)
 
         if (!this.request) {
-            this.request = xhr = new XMLHttpRequest();
+          this.request = xhr = new XMLHttpRequest()
 
-            xhr.onreadystatechange = function () {
-                var data;
-                if (xhr.readyState === xhr.DONE &&
-                    xhr.status === 200) {
-                    if (type === 'json') {
-                        data = JSON.parse( xhr.responseText );
-                    } else if (['kml', 'gpx'].indexOf(type) !== -1) {
-                        data = window.toGeoJSON[ type ]( xhr.responseXML );
-                    }
-                    _this.json = data;
-                    _this.layer.addData( data );
-                    _this.fire('ready');
-                }
-            };
+          xhr.onreadystatechange = function () {
+            var data
+            if (xhr.readyState === xhr.DONE && xhr.status === 200) {
+              if (type === 'json') {
+                data = JSON.parse(xhr.responseText)
+              } else if (['kml', 'gpx'].indexOf(type) !== -1) {
+                data = window.toGeoJSON[type](xhr.responseXML)
+              }
+              _this.json = data
+              _this.layer.addData(data)
+              _this.fire('ready')
+            }
+          }
 
-            xhr.open('get', this._url, true);
+          xhr.open('get', this._url, true)
 
-            xhr.send();
+          xhr.send()
         }
-        this.fire('add');
-    },
+        this.fire('add')
+      },
 
-    eachLayer : function (fnc) {
-        this.layer.eachLayer( fnc );
-    },
+      eachLayer: function (fnc) {
+        this.layer.eachLayer(fnc)
+      },
 
-    setStyle : function (style) {
-        
-        this.layer.setStyle( style );
-    },
+      setStyle: function (style) {
+        this.layer.setStyle(style)
+      },
 
-    resetStyle : function (layer) {
-        this.layer.resetStyle( layer );
-    },
+      resetStyle: function (layer) {
+        this.layer.resetStyle(layer)
+      },
 
-    onRemove : function (map) {
-        this.map.removeLayer( this.layer );        
-    },
+      onRemove: function (map) {
+        this.map.removeLayer(this.layer)
+      },
 
-    getBounds : function () {
-        return this.layer.getBounds();
+      getBounds: function () {
+        return this.layer.getBounds()
+      },
+    })
+
+    L.ajaxGeoJson = function (url, options) {
+      return new L.AjaxGeoJSON(url, options)
     }
-});
-
-L.ajaxGeoJson = function( url, options ) {
-    return new L.AjaxGeoJSON( url, options );
-};
+  }
+})()
--- scripts/shortcode-helper.js
+++ scripts/shortcode-helper.js
@@ -1,65 +1,61 @@
-(function () {
+;(function () {
+  window.WPLeafletMapPlugin = window.WPLeafletMapPlugin || []
+  window.WPLeafletMapPlugin.push(initAdminShortcodes)
 
-	window.WPLeafletMapPlugin = window.WPLeafletMapPlugin || [];
-	window.WPLeafletMapPlugin.push( initAdminShortcodes );
+  function initAdminShortcodes() {
+    var map_input = document.getElementById('map-shortcode')
+    var marker_input = document.getElementById('marker-shortcode')
+    var map_1 = window.WPLeafletMapPlugin.maps[0]
+    var marker_1 = window.WPLeafletMapPlugin.markers[0]
 
-	function initAdminShortcodes () {
-		var map_input = document.getElementById('map-shortcode'),
-			marker_input = document.getElementById('marker-shortcode'),
-			map_1 = window.WPLeafletMapPlugin.maps[0],
-			marker_1 = window.WPLeafletMapPlugin.markers[0];
-		
-		function update_marker () {
-			var latlng = marker_1.getLatLng();
-			marker_input.value = '[leaflet-marker lat=' +
-				latlng.lat +
-				' lng=' + 
-				latlng.lng + 
-				']';
-		}
+    function update_marker() {
+      var latlng = marker_1.getLatLng()
+      marker_input.value =
+        '[leaflet-marker lat=' + latlng.lat + ' lng=' + latlng.lng + ']'
+    }
 
-		function update_map () {
-			var bounds = map_1.getBounds();
-			var latlng = bounds.getCenter();
-			map_input.value = '[leaflet-map lat=' +
-				latlng.lat +
-				' lng=' + 
-				latlng.lng + 
-				' zoom=' +
-				map_1.getZoom() +
-				']';
+    function update_map() {
+      var bounds = map_1.getBounds()
+      var latlng = bounds.getCenter()
+      map_input.value =
+        '[leaflet-map lat=' +
+        latlng.lat +
+        ' lng=' +
+        latlng.lng +
+        ' zoom=' +
+        map_1.getZoom() +
+        ']'
 
-			// update marker if outside of bounds
-			if (!bounds.contains(marker_1.getLatLng())) {
-				// move marker to center
-				marker_1.setLatLng(latlng);
-			}
-		}
+      // update marker if outside of bounds
+      if (!bounds.contains(marker_1.getLatLng())) {
+        // move marker to center
+        marker_1.setLatLng(latlng)
+      }
+    }
 
-		marker_1.on('drag', update_marker);
-		map_1.on('move', update_map);
+    marker_1.on('drag', update_marker)
+    map_1.on('move', update_map)
 
-		update_map();
-		update_marker();
+    update_map()
+    update_marker()
 
-		if (map_input.addEventListener) {
-			map_input.addEventListener('click', function () {
-				this.select();
-			});
+    if (map_input.addEventListener) {
+      map_input.addEventListener('click', function () {
+        this.select()
+      })
 
-			marker_input.addEventListener('click', function () {
-				this.select();
-			});
-		} else {
-			// IE 8
-			map_input.attachEvent('onclick', function () {
-				map_input.select();
-			});
+      marker_input.addEventListener('click', function () {
+        this.select()
+      })
+    } else {
+      // IE 8
+      map_input.attachEvent('onclick', function () {
+        map_input.select()
+      })
 
-			marker_input.attachEvent('onclick', function () {
-				marker_input.select();
-			});
-		}
-
-	};
-})();
+      marker_input.attachEvent('onclick', function () {
+        marker_input.select()
+      })
+    }
+  }
+})()
