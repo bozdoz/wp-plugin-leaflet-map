4d21bbb2fb0289af889625857102d3f02034afa3
bozdoz <howaboutben@gmail.com>
2023-10-30 20:27:20 -0300

    removes commented code

--- shortcodes/class.wms-shortcode.php
+++ shortcodes/class.wms-shortcode.php
@@ -2,7 +2,7 @@
 /**
  * Wms Shortcode
  *
- * Displays map with [leaflet-wms src="path/to/wms"] 
+ * Displays map with [leaflet-wms src="path/to/wms"]
  *
  * JavaScript equivalent : L.TileLayer.wms('path/to/wms?', {layer: 'layername', crs: L.CRS.EPSG3857});
  *
@@ -12,7 +12,7 @@
 
 // Exit if accessed directly
 if (!defined('ABSPATH')) {
-    exit;
+  exit();
 }
 
 require_once LEAFLET_MAP__PLUGIN_DIR . 'shortcodes/class.map-shortcode.php';
@@ -22,51 +22,51 @@ require_once LEAFLET_MAP__PLUGIN_DIR . 'shortcodes/class.map-shortcode.php';
  */
 class Leaflet_Wms_Shortcode extends Leaflet_Map_Shortcode
 {
-    /**
-     * Get HTML for shortcode
-     * 
-     * @param string $atts    or Array
-     * @param string $content produced by adding atts to JavaScript
-     * 
-     * @return string HTML script
-     */
-    protected function getHTML($atts='', $content=null)
-    {
-        
-        extract($this->getAtts($atts));
-        
-        if (!empty($address)) {
-            include_once LEAFLET_MAP__PLUGIN_DIR . 'class.geocoder.php';
-            $location = new Leaflet_Geocoder( $address );
-            $lat = $location->lat;
-            $lng = $location->lng;
-        }
-        
-        $lat_set = isset($lat) || isset($y);
-        $lng_set = isset($lng) || isset($x);
-        
-        $lat = empty($lat) ? ( empty($y) ? '0' : $y ) : $lat;
-        $lng = empty($lng) ? ( empty($x) ? '0' : $x ) : $lng;
-        
-        // validate lat/lng
-        $lat = $this->LM->filter_float($lat);
-        $lng = $this->LM->filter_float($lng);
+  /**
+   * Get HTML for shortcode
+   *
+   * @param string $atts    or Array
+   * @param string $content produced by adding atts to JavaScript
+   *
+   * @return string HTML script
+   */
+  protected function getHTML($atts = '', $content = null)
+  {
+    extract($this->getAtts($atts));
 
-        /* only required field for image map (src/source) */
-        $src = empty($src) ? '' : $src;
-        $source = empty($source) ? 'https://ows.mundialis.de/services/service?' : $source;
-        $source = empty($src) ? $source : $src;
-        
-        $layer = empty($layer) ? 'TOPO-OSM-WMS' : $layer;
-        
-        $crs = str_replace(':','', empty($crs) ? 'EPSG:3857': $crs);
-        
-        ob_start(); ?>/*<script>*/
+    if (!empty($address)) {
+      include_once LEAFLET_MAP__PLUGIN_DIR . 'class.geocoder.php';
+      $location = new Leaflet_Geocoder($address);
+      $lat = $location->lat;
+      $lng = $location->lng;
+    }
+
+    $lat_set = isset($lat) || isset($y);
+    $lng_set = isset($lng) || isset($x);
+
+    $lat = empty($lat) ? (empty($y) ? '0' : $y) : $lat;
+    $lng = empty($lng) ? (empty($x) ? '0' : $x) : $lng;
+
+    // validate lat/lng
+    $lat = $this->LM->filter_float($lat);
+    $lng = $this->LM->filter_float($lng);
+
+    /* only required field for image map (src/source) */
+    $src = empty($src) ? '' : $src;
+    $source = empty($source)
+      ? 'https://ows.mundialis.de/services/service?'
+      : $source;
+    $source = empty($src) ? $source : $src;
+
+    $layer = empty($layer) ? 'TOPO-OSM-WMS' : $layer;
+
+    $crs = str_replace(':', '', empty($crs) ? 'EPSG:3857' : $crs);
+
+    ob_start();
+    ?>/*<script>*/
 var options = L.Util.extend({}, {
         attributionControl: false
-    }, <?php echo $map_options; ?>, {
-//         crs: L.CRS.EPSG:3857 //default
-    });
+    }, <?php echo $map_options; ?>);
 var zoom = <?php echo $zoom; ?>;
 var map = window.WPLeafletMapPlugin.createMap(options).setView(L.latLng(<?php echo $lat; ?>, <?php echo $lng; ?>), zoom);
 var wmslayer = L.tileLayer.wms(
@@ -78,9 +78,9 @@ var wmslayer = L.tileLayer.wms(
 ).addTo( map );
 
 <?php
+$script = ob_get_clean();
 
-        $script = ob_get_clean();
-
-        return $this->getDiv($height, $width) . $this->wrap_script($script, 'WPLeafletWmsShortcode');
-    }
+return $this->getDiv($height, $width) .
+  $this->wrap_script($script, 'WPLeafletWmsShortcode');
+  }
 }
